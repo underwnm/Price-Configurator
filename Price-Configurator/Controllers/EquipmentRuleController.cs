4bc1622b211602566ac3544e6788922a262ce6ca
underwnm <underwnm@gmail.com>
2017-01-04 12:52:58 -0600

    Implement page for equipment rules.

---
+++ Price-Configurator/Controllers/EquipmentRuleController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Price_Configurator.Models;
+using Price_Configurator.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Price_Configurator.Controllers
+{
+    public class EquipmentRuleController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public EquipmentRuleController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Rule
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                if (!IsAdminUser())
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            var model = new EquipmentRuleViewModel()
+            {
+                CurrentEquipmentRules = _context.EquipmentRules.ToList(),
+            };
+            return View(model);
+        }
+
+        public bool IsAdminUser()
+        {
+            if (!User.Identity.IsAuthenticated) return false;
+            var user = User.Identity;
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(_context));
+            var roles = userManager.GetRoles(user.GetUserId());
+            return roles[0] == "Admin";
+        }
+    }
+}
