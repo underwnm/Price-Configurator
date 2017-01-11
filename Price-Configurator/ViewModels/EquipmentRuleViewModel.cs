7aa1aa3498479514e9a6bd81552ab9fdc5873e94
underwnm <underwnm@gmail.com>
2017-01-11 13:52:26 -0600

    Change look and feel of all admin pages.

--- Price-Configurator/ViewModels/EquipmentRuleViewModel.cs
+++ Price-Configurator/ViewModels/EquipmentRuleViewModel.cs
@@ -9,16 +9,16 @@ namespace Price_Configurator.ViewModels
         public IEnumerable<EquipmentRule> CurrentEquipmentRules { get; set; }
 
         [Required]
-        [Display(Name = "Rule Name")]
+        [Display(Name = "Rule Name*")]
         public string Name { get; set; }
 
         [Required]
-        [Display(Name = "Parent Equipment")]
+        [Display(Name = "Parent Equipment*")]
         public int ParentEquipmentId { get; set; }
         public IEnumerable<Equipment> ParentEquipment { get; set; }
 
         [Required]
-        [Display(Name = "Child Equipment")]
+        [Display(Name = "Child Equipment*")]
         public int ChildEquipmentId { get; set; }
         public IEnumerable<Equipment> ChildEquipment { get; set; }
     }
